8c6ae919cea261c3418082dd6bea631e29b3efaa
SheikhaAlenezi <shei5ha98@gmail.com>
2025-09-07 21:06:01 +0300

    ..

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,54 +1,53 @@
-// import { NextFunction, Request, Response } from "express";
-// import jwt from "jsonwebtoken";
-// import User from "../models/User";
-// import { JWT_SECRET, JWT_EXP } from "../config/database";
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+import { JWT_SECRET, JWT_EXP } from "../config/database";
 
-// interface ITokenPayload {
-//   _id: string;
-//   username: string;
-// }
+interface ITokenPayload {
+  _id: string;
+  username: string;
+}
 
-// export const authenticate = async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ): Promise<void> => {
-//   try {
-//     // first checking token is header
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//       res
-//         .status(401)
-//         .json({ message: "not authorized , missing authorization headers" });
-//       return;
-//     }
-//     //second format
-//     const [_, token] = authHeader.split(" ");
-//     console.log(token);
+export const authenticate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      res
+        .status(401)
+        .json({ message: "not authorized , missing authorization headers" });
+      return;
+    }
 
-//     // user id extract
+    const [_, token] = authHeader.split(" ");
 
-//     const decodedToken = jwt.verify(token, JWT_SECRET); //as ITokenPayload
-//     const { _id } = decodedToken;
-//     const user = await User.findById(_id);
+    if (!token) {
+      return next({ status: 401, message: "invalid" });
+    }
+    const decodedToken: any = jwt.verify(token, JWT_SECRET);
+    const { _id } = decodedToken;
+    const user = await User.findById(_id);
 
-//     if (!user) {
-//       return next({
-//         status: 401,
-//         message: "not authorized,missing auth headers",
-//       });
-//     }
-//     // update rquest w/h the user
-//     req.user = user;
+    if (!user) {
+      return next({
+        status: 401,
+        message: "not authorized,missing auth headers",
+      });
+    }
 
-//     console.log(decodedToken);
+    req.user = user;
 
-//     next();
-//   } catch (err) {
-//     console.log(err);
-//     return next({
-//       status: 401,
-//       message: "invalid signature",
-//     });
-//   }
-// };
+    console.log(decodedToken);
+
+    next();
+  } catch (err) {
+    console.log(err);
+    return next({
+      status: 401,
+      message: "invalid signature",
+    });
+  }
+};
